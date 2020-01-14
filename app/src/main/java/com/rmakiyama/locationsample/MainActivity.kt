f6dd017b250e723119defc5f445fb2e8ff7612e3
rmakiyama <rmakiyama.io@gmail.com>
2020-01-14 19:00:44 +0900

    ロケーション取得

--- app/src/main/java/com/rmakiyama/locationsample/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/locationsample/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.rmakiyama.locationsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+import timber.log.Timber
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLocationWithPermissionCheck()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @SuppressLint("MissingPermission")
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun getLocation() {
+        lifecycleScope.launch {
+            runCatching { fusedLocationClient.lastLocation.await() }
+                .onSuccess { location -> Timber.i("location => $location") }
+                .onFailure { Timber.e(it) }
+        }
     }
 }
